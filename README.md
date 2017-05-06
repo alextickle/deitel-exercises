37d304b9287f0b60c763bb694e78f332c67aabe6
alextickle <alexander.tickle@gmail.com>
2017-05-06 12:41:10 -0700

    added readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# deitel-exercises
+
+My solutions to the exercises in 'Java How to Program, 10/e (Early Objects)' by Paul and Harvey Deitel.
--- deitel_ch1/Welcome1.java
+++
@@ -1,9 +0,0 @@
-package java_I;
-public class Welcome1 {
-
-	public static void main(String[] args) {
-		System.out.println("Welcome to Java Programming!");
-
-	}
-
-}
